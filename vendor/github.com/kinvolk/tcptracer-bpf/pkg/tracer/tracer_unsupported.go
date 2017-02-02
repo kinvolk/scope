1c82e3c060771b455b702557563295a03092f36e
Alban Crequy <alban@kinvolk.io>
2017-02-02 17:41:59 +0100

    vendor: update gobpf and tcptracer-bpf
    Based on WIP branches

--- vendor/github.com/kinvolk/tcptracer-bpf/pkg/tracer/tracer_unsupported.go
+++ vendor/github.com/kinvolk/tcptracer-bpf/pkg/tracer/tracer_unsupported.go
@@ -8,7 +8,7 @@ import (
 
 type Tracer struct{}
 
-func NewTracerFromFile(fileName string, tcpEventCbV4 func(TcpV4), tcpEventCbV6 func(TcpV6)) (*Tracer, error) {
+func NewTracer(tcpEventCbV4 func(TcpV4), tcpEventCbV6 func(TcpV6)) (*Tracer, error) {
 	return nil, fmt.Errorf("not supported on non-Linux systems")
 }
 
