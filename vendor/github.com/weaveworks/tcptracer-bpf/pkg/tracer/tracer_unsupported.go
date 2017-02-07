600f829d34157cf453243c665ba13bea31531921
Alban Crequy <alban@kinvolk.io>
2017-02-07 19:48:30 +0100

    vendor: update tcptracer-bpf
    Use the new repository: https://github.com/weaveworks/tcptracer-bpf
    Up to commit e8ea19b70513401d52360e9878b4bcea5e0217a3

--- vendor/github.com/weaveworks/tcptracer-bpf/pkg/tracer/tracer_unsupported.go
+++ vendor/github.com/weaveworks/tcptracer-bpf/pkg/tracer/tracer_unsupported.go
@@ -8,6 +8,10 @@ import (
 
 type Tracer struct{}
 
+func TracerAsset() ([]byte, error) {
+	return nil, fmt.Errorf("not supported on non-Linux systems")
+}
+
 func NewTracer(tcpEventCbV4 func(TcpV4), tcpEventCbV6 func(TcpV6)) (*Tracer, error) {
 	return nil, fmt.Errorf("not supported on non-Linux systems")
 }
