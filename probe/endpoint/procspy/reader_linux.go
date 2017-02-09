60faaf831d207df0f5d6f32a75781da78bf54f76
Alessandro Puccetti <alessandro@kinvolk.io>
2017-02-09 12:01:25 +0100

    remove unnecessary mutex

--- probe/endpoint/procspy/reader_linux.go
+++ probe/endpoint/procspy/reader_linux.go
@@ -105,7 +105,6 @@ func (br *backgroundReader) loop(walker process.Walker) {
 
 type foregroundReader struct {
 	stopc         chan struct{}
-	mtx           sync.Mutex
 	latestBuf     *bytes.Buffer
 	latestSockets map[uint64]*Proc
 }
@@ -125,10 +124,8 @@ func newForegroundReader(walker process.Walker) reader {
 	go performWalk(pWalker, walkc)
 
 	result := <-walkc
-	fr.mtx.Lock()
 	fr.latestBuf = result.buf
 	fr.latestSockets = result.sockets
-	fr.mtx.Unlock()
 
 	return fr
 }
@@ -138,8 +135,6 @@ func (fr *foregroundReader) stop() {
 }
 
 func (fr *foregroundReader) getWalkedProcPid(buf *bytes.Buffer) (map[uint64]*Proc, error) {
-	fr.mtx.Lock()
-	defer fr.mtx.Unlock()
 	// Don't access latestBuf directly but create a reader. In this way,
 	// the buffer will not be empty in the next call of getWalkedProcPid
 	// and it can be copied again.
