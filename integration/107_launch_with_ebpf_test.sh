d7c93961cf6f716b26d4ce81d2a8e37b7e3aec41
Michael Schubert <michael@kinvolk.io>
2017-02-14 18:52:50 +0100

    Add 107_launch_with_ebpf_test.sh

---
+++ integration/107_launch_with_ebpf_test.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# shellcheck disable=SC1091
+. ./config.sh
+
+start_suite "Launch scope and check it boots, with ebpf connection tracking enabled"
+
+scope_on "$HOST1" launch --probe.ebpf.connections=true
+
+wait_for_containers "$HOST1" 60 weavescope
+
+has_container "$HOST1" weavescope
+
+scope_end_suite
