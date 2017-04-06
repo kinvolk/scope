3526dc8cd2930c170234737a73bd88eecab44a0d
Alban Crequy <alban@kinvolk.io>
2017-04-06 12:52:52 +0200

    integration: attempt to reproduce issue 2308 (debugging)

--- integration/301_internet_edge_with_ebpf_test.sh
+++ integration/301_internet_edge_with_ebpf_test.sh
@@ -7,6 +7,14 @@ start_suite "Test short lived connections from the Internet"
 
 weave_on "$HOST1" launch
 scope_on "$HOST1" launch --probe.ebpf.connections=true
+
+#for i in $(seq 1 20) ; do
+#    ps aux | grep '[i]ptables' || true
+#    cat /proc/net/unix | grep 'xtables' || true
+#done
+
+set -x
+
 docker_on "$HOST1" run -d -p 80:80 --name nginx nginx
 
 do_connections() {
