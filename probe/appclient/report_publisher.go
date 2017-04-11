37cc006fd58a0c58c4fbb6c74e12f2b93cb2cd2c
Alban Crequy <alban@kinvolk.io>
2017-04-11 16:16:35 +0200

    report: test various level of compression

--- probe/appclient/report_publisher.go
+++ probe/appclient/report_publisher.go
@@ -3,7 +3,11 @@ package appclient
 import (
 	"bytes"
 	"compress/gzip"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/weaveworks/scope/report"
+	"io/ioutil"
+	"time"
 )
 
 // A ReportPublisher uses a buffer pool to serialise reports, which it
@@ -28,6 +32,21 @@ func (p *ReportPublisher) Publish(r report.Report) error {
 			t.Controls = report.Controls{}
 		})
 	}
+
+	for level := gzip.DefaultCompression; level <= gzip.BestCompression; level++ {
+		allTmpBuf := []*bytes.Buffer{}
+		start := time.Now()
+		for i := 0; i < 10; i++ {
+			tmpBuf := &bytes.Buffer{}
+			r.WriteBinary(tmpBuf, level)
+			allTmpBuf = append(allTmpBuf, tmpBuf)
+		}
+		elapsed := time.Since(start)
+
+		log.Infof("Report: level=%d Len=%d Elapsed=%s", level, allTmpBuf[0].Len(), elapsed)
+		ioutil.WriteFile(fmt.Sprintf("/tmp/report_%d", level), allTmpBuf[0].Bytes(), 0644)
+	}
+
 	buf := &bytes.Buffer{}
 	r.WriteBinary(buf, gzip.BestCompression)
 	return p.publisher.Publish(buf)
